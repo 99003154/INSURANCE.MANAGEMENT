baad30eb2f3e1aea6d64da29b36a8ec579255787
Madhushree-C <69195097+Madhushree-C@users.noreply.github.com>
2020-09-27 19:40:30 +0530

    Update insurance_management.h

--- inc/insurance_management.h
+++ inc/insurance_management.h
@@ -1,4 +1,4 @@
-/** 
+/**
 * @file insurance_management.h
 * Insurance management system with various operation
 *
@@ -8,25 +8,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio_ext.h>
 
 /**
 * checks whether operand1 contains only character or not
-* @param[in] operand1  
+* @param[in] operand1
 * @return Result retruns 1 if operand1 only contains character else returns 0
 */
-char username(char operand1);
+char username(char operand1[]);
 
 /**
 *  checks whether operand1 contains only character or not
 * @param[in] operand1
 * @return Result retruns 1 if operand1 only contains character else returns 0
 */
-char password(char operand1);
+char password(char operand2[]);
 
 /**
 *  checks whether operand1 is between 18 to 100
-* @param[in] operand1 
+* @param[in] operand1
 * @return Result retruns 1 if operand1 is between 18 to 100 else returns 0
 */
 int age(int operand1);
@@ -34,39 +33,39 @@ int age(int operand1);
 
 /**
 * checks whether operand1 as 10 digits
-* @param[in] operand1 
+* @param[in] operand1
 * @return Result retruns 1 if operand1 as 10 digits else returns 0
 */
-int phonenumber(int operand1);
+int phonenumber(char operand4[]);
 
 
 /**
 * checks whether operand1 is in size of 15(characters and digits)
-* @param[in] operand1 
+* @param[in] operand1
 * @return Result retruns 1 if operand1 is in size of 15(characters and digits) else returns 0
 */
-char licensenumber(char operand1);
+char licensenumber(char operand5[]);
 
 /**
 * checks whether operand1 is in size of 10(characters and digits)
-* @param[in] operand1 
+* @param[in] operand1
 * @return Result retruns 1 if operand1 is in size of 10(characters and digits) else returns 0
 */
-int enginenumber(int operand1);
+char enginenumber(int operand6[]);
 
 /**
 * tells whether cliend pays the principle amount or quits the portal
-* @param[in] operand1 
+* @param[in] operand1
 * @return Result if operand1 is 1 client pays the principle amount else quit the portal
 */
-int principleamount(int operand1);
+char pay_pa(int operand7);
 
-/**
+/*//**
 * tells whether cliend pays the specified principle amount
-* @param[in] operand1 
+* @param[in] operand1
 * @return Result returns 1 if correct principle amount is paid else return 0
 */
-int correctpayment(int operand1);
+//char pa_payment(int amnt);
 
 
 
