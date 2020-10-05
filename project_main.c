b57cce10091066abb19491a13a79e61e5d6da78f
Madhushree-C <69195097+Madhushree-C@users.noreply.github.com>
2020-10-05 11:22:11 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -8,20 +8,10 @@
 unsigned int insurance_operation = 0;
 
 /* Operands on which calculation is performed */
-char insurance_operand10;
-char insurance_operand1[];
-char insurance_operand2[];
-char insurance_operand9;
-int insurance_operand8;
-char insurance_operand4[];
-char insurance_operand3;
-char insurance_operand5[];
-char insurance_operand6[];
-int insurance_operand7;
-char insurance_user_name;
-char insurance_pwd;
-char insurance_id[];
-
+char insurance_op1 = 0;
+char insurance_op2[] = 0;
+char insurance_op4[] = 0;
+int insurance_op3 = 0;
 /* Valid operations */
 enum operations{ CHOICE=1, USERNAME, PASSWORD, GENDER, AGE, PHONENUMBER, VEHICLETYPE, LINCENSENUMBER, ENGINENUMBER, PAY_PA, LOGIN, CLAIM, EXIT };
 
@@ -60,7 +50,7 @@ void insurance_menu(void)
     {
         printf("\n\tEnter the information with space between them\n");
         __fpurge(stdin);
-        scanf("%c %s %s %c %d %s %c %s %s %d %c %c %s", &insurance_operand10, &insurance_operand1, &insurance_operand2, &insurance_operand9,&insurance_operand8, &insurance_operand4, &insurance_operand3, &insurance_operand5, &insurance_operand6, &insurance_operand7, &insurance_user_name, &insurance_pwd, &insurance_id);
+        scanf("%c %s %s %d", &insurance_op1, &insurance_op2, &insurance_op4, &insurance_op3);
     }
     else
     {
@@ -74,97 +64,97 @@ void insurance_menu(void)
     {
         case CHOICE:
             printf("\n\t%d + %d = %d\nEnter to continue", 
-            insurance_operand10, 
-            choice(insurance_operand10));
+            insurance_op1, 
+            choice(insurance_op1));
             
             __fpurge(stdin);
             getchar();
             break;
         case USERNAME:
             printf("\n\t%d - %d = %d\nEnter to continue", 
-            insurance_operand1[],
-            username(insurance_operand1[]));
+            insurance_op2[],
+            username(insurance_op2[]));
             
             __fpurge(stdin);
             getchar();
             break;
         case PASSWORD:
             printf("\n\t%d * %d = %d\nEnter to continue", 
-            insurance_operand2[], 
-            password(insurance_operand2[]));
+            insurance_op2[], 
+            password(insurance_op2[]));
             
             __fpurge(stdin);
             getchar();
             break;
         case GENDER:
             printf("\n\t%d / %d = %d\nEnter to continue", 
-            insurance_operand9, 
-            gender(insurance_operand9));
+            insurance_op1, 
+            gender(insurance_op1));
             
             __fpurge(stdin);
             getchar();
             break;
         case AGE:
             printf("\n\t Greater %d and %d = %d\nEnter to continue", 
-            insurance_operand8, 
-            age(insurance_operand8));
+            insurance_op3, 
+            age(insurance_op3));
             
             __fpurge(stdin);
             getchar();
             break;
         case PHONENUMBER:
             printf("\n\t Primenum %d =%d\nEnter to continue", 
-            insurance_operand4[], 
-            phonenumber(insurance_operand4[]));
+            insurance_op2[], 
+            phonenumber(insurance_op2[]));
             
             __fpurge(stdin);
             getchar();
             break;
         case VEHICLETYPE:
             printf("\n\t Primenum1 %d =%d\nEnter to continue", 
-            insurance_operand3, 
-            vehicletype(insurance_operand3));
+            insurance_op1, 
+            vehicletype(insurance_op1));
             
             __fpurge(stdin);
             getchar();
             break;
           case LICENSENUMBER:
             printf("\n\t evenodd %d =%d\nEnter to continue", 
-            insurance_operand5[], 
-            licensenumber(insurance_operand5[]));
+            insurance_op2[], 
+            licensenumber(insurance_op2[]));
             
             __fpurge(stdin);
             getchar();
             break;
           case ENGINENUMBER:
             printf("\n\t evenodd1 %d =%d\nEnter to continue", 
-            insurance_operand6[10], 
-            enginenumber(insurance_operand6[10]));
+            insurance_op2[], 
+            enginenumber(insurance_op2[]));
             
             __fpurge(stdin);
             getchar();
             break;
           case PAY_PA:
             printf("\n\t posneg %d =%d\nEnter to continue", 
-            insurance_operand7, 
-            pay_pa(insurance_operand7));
+            insurance_op3, 
+            pay_pa(insurance_op3));
             
             __fpurge(stdin);
             getchar();
             break;
         case LOGIN:
             printf("\n\t posneg1 %d =%d\nEnter to continue", 
-            insurance_username, 
-            insurance_pwd,
-            login(insurance_username, insurance_pwd));
+            insurance_op2[], 
+            insurance_op4[],
+            login(insurance_op2[], insurance_op4[]));
             
             __fpurge(stdin);
             getchar();
             break;
         case CLAIM:
             printf("\n\t factorial %d =%d\nEnter to continue", 
-            insurance_id[], 
-            claim(insurance_id[]));
+            insurance_op2[], 
+            claim(insurance_op2[]));
             
             __fpurge(stdin);
             getchar();
